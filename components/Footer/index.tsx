226d02293af9332a10d5d4c8b9d5f91726a536cf
Jordan <jordan@Jordans-MacBook-Air.local>
2022-03-31 19:53:13 -0400

    update footer emoji

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,5 +1,5 @@
 export const Footer = () => (
    <footer className="footer">
-      <p>Designed & Developed by Jordan Utz in St. Petersburg, Florida 🍃</p>
+      <p>Designed & Developed by Jordan Utz in St. Petersburg, Florida 🪁</p>
    </footer>
 );
